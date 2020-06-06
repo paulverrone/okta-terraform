ccc6b16d99228d7ab977b3d97193e60c27a902e7
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-05 20:50:11 -0400

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -8,19 +8,33 @@ provider "okta" {
     api_token = var.api_token
 }
 
+/*
 resource "okta_user" "okta_terraform_admin" {
-  first_name = "Okta"
-  last_name  = "Terraform Admin"
-  login      = "OktaTerraformAdmin@iatcore.com"
-  email      = "OktaTerraformAdmin@iatcore.com"
-  status     = "ACTIVE"
-  admin_roles = ["SUPER_ADMIN"]
+    first_name = "Okta"
+    last_name  = "Terraform Admin"
+    login      = "OktaTerraformAdmin@iatcore.com"
+    email      = "OktaTerraformAdmin@iatcore.com"
+    status     = "ACTIVE"
+    admin_roles = ["SUPER_ADMIN"]
+}
+*/
+    
+data "okta_user" "okta_terraform_admin" {
+   search {
+       name = profile.firstName
+       value = "Okta"
+   }
+   search {
+       name = profile.lastName
+       value = "Terraform Admin"
+   }
 }
 
+
 resource "okta_group" "service_accounts" {
-  name        = "Okta Service Accounts"
-  description = "Groups that contains all users that represent Service Accounts"
-  users = [
-    "${okta_user.okta_terraform_admin.id}"
-  ]
+    name        = "Okta Service Accounts"
+    description = "Groups that contains all users that represent Service Accounts"
+    users = [
+        "${okta_user.okta_terraform_admin.id}"
+    ]
 }
