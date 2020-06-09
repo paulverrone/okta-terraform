4c32d33bb03ec34d8e3ac2382a5c87261671fbe5
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 17:37:40 -0400

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -3,11 +3,11 @@ resource "okta_policy_signon" "signon_always_mfa" {
   name            = "Always MFA"
   status          = "ACTIVE"
   description     = "Always MFA for a subset of users"
-  groups_included = ["${data.okta_group.dmzadmin_group.id}"]
+  groups_included = ["data.okta_group.dmzadmin_group.id"]
 }
 
 resource "okta_policy_rule_signon" "signon_rule_always_mfa" {
-  policyid = "${okta_policy_signon.signon_always_mfa.id}"
+  policyid = "okta_policy_signon.signon_always_mfa.id"
   name     = "Always MFA"
   status   = "ACTIVE"
   mfa_required = "true"
