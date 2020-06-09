2675653c946043629d3da05363c7690338102723
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 17:28:46 -0400

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -1,1 +1,13 @@
+# Custom sign-on policies
+resource "okta_policy_signon" "signon_always_mfa" {
+  name            = "Always MFA"
+  status          = "ACTIVE"
+  description     = "Always MFA for a subset of users"
+  groups_included = ["${data.okta_group.everyone.id}"]
+}
 
+resource "okta_policy_rule_signon" "signon_rule_always_mfa" {
+  policyid = "${okta_policy_signon.signon_always_mfa.id}"
+  name     = "Always MFA"
+  status   = "ACTIVE"
+}
