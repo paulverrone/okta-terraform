d8398e07a877a9481902c780a71a2d93a2e4beff
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 14:13:06 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -13,7 +13,7 @@ resource "okta_app_auto_login" "corporate_linkedin" {
   label          = "LinkedIn - Shared Corporate Account"
   preconfigured_app = "linkedin"
   credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
-  shared_username = "ripley.dalton@iatcore.com"
+https://github.com/paulverrone  shared_username = "ripley.dalton@iatcore.com"
   shared_password = "t+#_d!VhM$N2j@JS"
   lifecycle {
     ignore_changes = [groups]
@@ -26,6 +26,15 @@ resource "okta_app_auto_login" "corporate_facebook" {
   credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
   shared_username = "iatcore"
   shared_password = "t+#_d!VhM$N2j@JS"
+  lifecycle {
+    ignore_changes = [groups]
+  }
+}
+
+resource "okta_app_auto_login" "github" {
+  label          = "GitHub Team"
+  preconfigured_app = "github team"
+  credentials_scheme = "EDIT_USERNAME_AND_PASSWORD"
   lifecycle {
     ignore_changes = [groups]
   }
