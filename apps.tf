a9cb3107d682668a677354d653e94c3f4b974545
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 12:56:59 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -1,1 +1,7 @@
-
+resource "okta_app_swa" "corporate_twitter" {
+  label          = "Twitter - Shared Corporate Account"
+  preconfigured_app = "twitter"
+  credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
+  shared_username = "iatcore"
+  shared_password = "t+#_d!VhM$N2j@JS"
+}
