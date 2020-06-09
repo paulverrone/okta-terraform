fabdad4837ed763bedd49481847f18bce281bf21
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 13:22:36 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -8,3 +8,25 @@ resource "okta_app_auto_login" "corporate_twitter" {
     ignore_changes = [groups]
   }
 }
+
+resource "okta_app_auto_login" "corporate_linkedin" {
+  label          = "LinkedIn - Shared Corporate Account"
+  preconfigured_app = "linkedin"
+  credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
+  shared_username = "iatcore"
+  shared_password = "t+#_d!VhM$N2j@JS"
+  lifecycle {
+    ignore_changes = [groups]
+  }
+}
+
+resource "okta_app_auto_login" "corporate_facebook" {
+  label          = "Facebook - Shared Corporate Account"
+  preconfigured_app = "facebook"
+  credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
+  shared_username = "iatcore"
+  shared_password = "t+#_d!VhM$N2j@JS"
+  lifecycle {
+    ignore_changes = [groups]
+  }
+}
