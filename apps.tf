c11e0fa6de2c6e827117979efdd4c7aa8c9229df
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 12:58:05 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -1,4 +1,4 @@
-resource "okta_app_swa" "corporate_twitter" {
+resource "okta_app_secure_password_store" "corporate_twitter" {
   label          = "Twitter - Shared Corporate Account"
   preconfigured_app = "twitter"
   credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
