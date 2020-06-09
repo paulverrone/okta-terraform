4a4b1e3c4e1d235fd1cc11e00a0f119d644ebb1e
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-09 13:04:12 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -4,4 +4,7 @@ resource "okta_app_auto_login" "corporate_twitter" {
   credentials_scheme = "SHARED_USERNAME_AND_PASSWORD"
   shared_username = "iatcore"
   shared_password = "t+#_d!VhM$N2j@JS"
+  lifecycle {
+    ignore_changes = ["groups"]
+  }
 }
