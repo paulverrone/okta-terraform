c870c23052d1a8c80715d44c92c177460f91d7c8
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-08 20:29:43 -0400

    Delete identity.tf

--- identity.tf
+++
@@ -1,9 +0,0 @@
-
-
-# Configure recommended sign-on, MFA, and password policies
-
-# Configure sign-on and MFA policy for "Okta Service Accounts" group
-
-# Configure API Authorization Server
-
-# Set up common apps
