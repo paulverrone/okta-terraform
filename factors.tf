c5b9ff434959a22081db0090be393bf1b123b489
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 15:05:07 -0400

    Update factors.tf

--- factors.tf
+++ factors.tf
@@ -11,9 +11,11 @@ resource "okta_factor" "factor_okta_call" {
   provider_id = "okta_call"
 }
 
+/* Provider does not support this factor type yet
 resource "okta_factor" "factor_okta_email" {
   provider_id = "okta_email"
 }
+*/
   
 resource "okta_factor" "factor_google_authenticator" {
   provider_id = "google_otp"
