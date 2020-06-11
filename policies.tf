6c70d675d76b45b26065a2020988b329ee76545f
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 15:40:06 -0400

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -1,3 +1,40 @@
+# Custom MFA Enrollment Policy and rule
+resource "okta_policy_mfa" "recruiting_mfa_policy" {
+  name        = "Recruiting MFA"
+  status      = "ACTIVE"
+  description = "MFA enrollment policy for Recruiting"
+
+  okta_otp = {
+    enroll = "REQUIRED"
+  }
+
+  google_otp = {
+    enroll = "NOT_ALLOWED"
+  }
+  
+   okta_call = {
+    enroll = "NOT_ALLOWED"
+  } 
+
+  groups_included = ["data.okta_group.recruiting_group.id"]
+  
+  depends_on = [
+    "okta_factor.factor_okta_verify",
+    "okta_factor.factor_okta_call",
+    "okta_factor.factor_google_authenticator"
+  ]
+}
+
+resource okta_policy_rule_mfa marketing_mfa {
+  policyid           = "${okta_policy_signon.test.id}"
+  name               = "MFA Enrollment"
+  status             = "ACTIVE"
+  enroll             = "LOGIN"
+  network_connection = "ZONE"
+  network_excludes   = ["${okta_network_zone.blacklisted_countries.id}"]
+}
+
+
 # Custom sign-on policies
 resource "okta_policy_signon" "signon_always_mfa" {
   name            = "Always MFA"
