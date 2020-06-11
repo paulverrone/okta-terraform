599d9bff90787555a87e8f0379e792cbe64f35c2
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 18:21:05 -0400

    Update users-and-groups.tf

--- users-and-groups.tf
+++ users-and-groups.tf
@@ -43,7 +43,7 @@ data "okta_group" "dmzadmin_group" {
 
 # Add recruiting group to apps
 resource "okta_app_group_assignment" "assign_twitter_recruiting" {
-  app_id   = okta_app_auto_login.twitter.id
+  app_id   = "resource.okta_app_auto_login.twitter.id"
   group_id = data.okta_group.recruiting_group.id
 }
 
