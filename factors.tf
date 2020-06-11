68acf978da72dc0164d15208a7c710818d8a0737
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 14:51:24 -0400

    Update factors.tf

--- factors.tf
+++ factors.tf
@@ -1,20 +1,19 @@
 resource "okta_factor" "factor_okta_verify" {
-  provider = "okta_otp"
+  provider = okta_otp
 }
 
 resource "okta_factor" "factor_okta_push" {
-  provider = "okta_push"
+  provider = okta_push
 }
 
 resource "okta_factor" "factor_okta_call" {
-  provider = "okta_call"
+  provider = okta_call
 }
 
 resource "okta_factor" "factor_okta_email" {
-  provider = "okta_email"
+  provider = okta_email
 }
 
-
 resource "okta_factor" "factor_google_authenticator" {
-  provider = "google_otp"
+  provider = google_otp
 }
