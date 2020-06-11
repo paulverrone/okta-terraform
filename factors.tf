05d9abdc5b9b9ad687ff9d589079fff3b2d15a6c
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 14:59:53 -0400

    Update factors.tf

--- factors.tf
+++ factors.tf
@@ -17,6 +17,5 @@ resource "okta_factor" "factor_okta_email" {
 */
   
 resource "okta_factor" "factor_google_authenticator" {
-  provider = "google_otp"
-  active = "true"
+  provider = okta.GoogleOtpFactor
 }
