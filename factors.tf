fce7f68fbb76e7af66e95a70496baf3c5567b771
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-11 14:50:17 -0400

    Create factors.tf

---
+++ factors.tf
@@ -0,0 +1,20 @@
+resource "okta_factor" "factor_okta_verify" {
+  provider = "okta_otp"
+}
+
+resource "okta_factor" "factor_okta_push" {
+  provider = "okta_push"
+}
+
+resource "okta_factor" "factor_okta_call" {
+  provider = "okta_call"
+}
+
+resource "okta_factor" "factor_okta_email" {
+  provider = "okta_email"
+}
+
+
+resource "okta_factor" "factor_google_authenticator" {
+  provider = "google_otp"
+}
