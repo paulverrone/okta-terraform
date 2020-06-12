b3a72b8e6a61056591d5940c6ecbab89fd44a5f6
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-12 13:38:42 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -31,6 +31,15 @@ resource "okta_app_auto_login" "corporate_facebook" {
   }
 }
 
+resource "okta_app_oauth" "oidc_sample" {
+  status         = "ACTIVE"
+  label          = "OIDC Demo Application"
+  type           = "web"
+  redirect_uris  = ["https://super-widget.oktaprise.com"]
+  login_uri      = "https://super-widget.oktaprise.com"
+  grant_types    = ["implicit", "authorization_code"]
+}
+
 /* Need to determine how to specify Github Organization Name
 resource "okta_app_auto_login" "github" {
   label          = "GitHub Team"
