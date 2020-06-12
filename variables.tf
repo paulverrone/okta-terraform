f7b7486d6319b019af723de5eeb1558f4fbba1fc
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-12 16:49:27 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,5 @@ variable "org_name" {}
 variable "api_token" {}
 variable "base_url" {}
 variable "shared_admin_password_swa" {}
+variable "sso_google_client_id" {}
+variable "sso_google_client_secret" {}
