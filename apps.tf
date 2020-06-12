b0e35a781a9d2246171bc36470df004540fcad94
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-12 14:20:34 -0400

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -38,6 +38,7 @@ resource "okta_app_oauth" "oidc_sample" {
   redirect_uris  = ["https://super-widget.oktaprise.com"]
   login_uri      = "https://super-widget.oktaprise.com"
   grant_types    = ["implicit", "authorization_code", "refresh_token"]
+  response_types = ["code", "token"]
   consent_method = "TRUSTED"
   lifecycle {
     ignore_changes = [users, groups]
