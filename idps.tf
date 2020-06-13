8a1ac06a99ba379c2f642c4232ce7d2dd307dbd0
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-06-12 21:12:51 -0400

    Update idps.tf

--- idps.tf
+++ idps.tf
@@ -14,3 +14,33 @@ resource "okta_idp_social" "google" {
   #subject_match_type        = "CUSTOM_ATTRIBUTE"
   #subject_match_attribute   = "customfieldId"
 }
+
+resource "okta_idp_social" "facebook" {
+  type          = "FACEBOOK"
+  protocol_type = "OAUTH2"
+  name          = "Facebook IdP"
+
+  scopes = [
+    "public_profile",
+    "email",
+  ]
+
+  client_id         = "abcd123"
+  client_secret     = "abcd123"
+  username_template = "idpuser.email"
+}
+
+resource "okta_idp_social" "linkedin" {
+  type          = "LINKEDIN"
+  protocol_type = "OAUTH2"
+  name          = "LinkedIn IDP"
+
+  scopes = [
+    "r_liteprofile",
+    "r_emailaddress",
+  ]
+
+  client_id         = "abcd123"
+  client_secret     = "abcd123"
+  username_template = "idpuser.email"
+}
