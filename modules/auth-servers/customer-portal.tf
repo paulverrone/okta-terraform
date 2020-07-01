d7dfd9b757ffa68a8e3fd817e18f035f7b057ae7
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-01 12:23:31 -0400

    Update customer-portal.tf

--- modules/auth-servers/customer-portal.tf
+++ modules/auth-servers/customer-portal.tf
@@ -10,7 +10,7 @@ resource "okta_auth_server_scope" "account_read" {
   auth_server_id = okta_auth_server.customer_portal.id
   consent        = "REQUIRED"
   description    = "This allows the app to view your IATCore account information."
-  name           = "account.read"
+  name           = "iat.account.read"
   # Need ability to set the displayname for the scope
 }
 
