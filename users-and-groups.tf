862db8ed6d3c0f77ca34944b68f7beec850b8a7c
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 12:25:13 -0400

    Update users-and-groups.tf

--- users-and-groups.tf
+++ users-and-groups.tf
@@ -64,6 +64,10 @@ resource "okta_app_group_assignment" "assign_linkedin_recruiting" {
 
 resource "okta_app_group_assignment" "assign_oidc_example_all" {
     app_id = module.applications.oidc_superwidget_id 
-    #okta_app_oauth.oidc_sample.id
     group_id = data.okta_group.all_group.id
 }
+    
+resource "okta_app_group_assignment" "assign_customer_portal_all" {
+    app_id = module.applications.customer_portal_app_id 
+    group_id = data.okta_group.all_group.id
+}    
