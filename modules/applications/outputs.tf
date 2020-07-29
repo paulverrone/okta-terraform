76d6913feaeac8128cb0a6c44ef2c6c68eb7ef2f
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 11:45:58 -0400

    Update outputs.tf

--- modules/applications/outputs.tf
+++ modules/applications/outputs.tf
@@ -2,6 +2,10 @@ output "oidc_superwidget_id" {
   value = "${okta_app_oauth.okta_super_widget.id}"
   }
 
+output "customer_portal_app_id" {
+  value = "${okta_app_oauth.customer_portal_app.id}"
+  }
+
 output "swa_corporate_linkedin_id" {
   value = "${okta_app_auto_login.corporate_linkedin.id}"
   }
