bdedba56c7f88b5f6da7f477595b05ee71e6f793
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 15:31:36 -0400

    Update azure-apim.tf

--- modules/applications/azure-apim.tf
+++ modules/applications/azure-apim.tf
@@ -2,7 +2,8 @@ resource "okta_app_oauth" "azure_apim" {
   status         = "ACTIVE"
   label          = "Azure API Management"
   type           = "web"
-  redirect_uris  = ["https://iatcoreapim.azure-api.net/docs/services/okta-default-openid-connect-iatcore-preview/console/openidconnect/authorizationcode/callback"]
+  redirect_uris  = ["https://iatcoreapim.azure-api.net/docs/services/okta-default-openid-connect-iatcore-preview/console/openidconnect/authorizationcode/callback",
+                    "https://oauth.pstmn.io/v1/callback"]
   #post_logout_redirect_uris = ["https://localhost:44369"]
   #login_uri      = "https://localhost:44369/signin-oidc"
   grant_types    = ["client_credentials", "authorization_code"]
