5e986ad28821fce06f2b1bc2509e32ec977a20d7
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 11:25:51 -0400

    Create customer-portal.tf

---
+++ modules/applications/customer-portal.tf
@@ -0,0 +1,13 @@
+resource "okta_app_oauth" "customer_portal" {
+  status         = "ACTIVE"
+  label          = "Customer Portal"
+  type           = "web"
+  redirect_uris  = ["https://localhost:44369/signin-oidc", "https://localhost:44369/Customer"]
+  login_uri      = "https://localhost:44369/signin-oidc"
+  grant_types    = ["client_credentials", "authorization_code"]
+  response_types = ["code", "token", "id_token"]
+  consent_method = "TRUSTED"
+  lifecycle {
+    ignore_changes = [users, groups]
+  }
+}
