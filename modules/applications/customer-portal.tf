72b712afaa1b8f7605a1580d754eee911a016d3c
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 11:52:45 -0400

    Update customer-portal.tf

--- modules/applications/customer-portal.tf
+++ modules/applications/customer-portal.tf
@@ -5,7 +5,7 @@ resource "okta_app_oauth" "customer_portal" {
   redirect_uris  = ["https://localhost:44369/signin-oidc", "https://localhost:44369/Customer"]
   login_uri      = "https://localhost:44369/signin-oidc"
   grant_types    = ["client_credentials", "authorization_code"]
-  response_types = ["code", "token"]
+  response_types = ["code"]
   consent_method = "TRUSTED"
   lifecycle {
     ignore_changes = [users, groups]
