b5db14db8eade10613f6a5beef39b7bd7e496b94
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-29 15:06:05 -0400

    Update azure-apim.tf

--- modules/applications/azure-apim.tf
+++ modules/applications/azure-apim.tf
@@ -7,7 +7,7 @@ resource "okta_app_oauth" "azure_apim" {
   #login_uri      = "https://localhost:44369/signin-oidc"
   grant_types    = ["client_credentials", "authorization_code"]
   response_types = ["code"]
-  #consent_method = "TRUSTED"
+  consent_method = "TRUSTED"
   lifecycle {
     ignore_changes = [users, groups]
   }
