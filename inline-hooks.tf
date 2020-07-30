e08d8c394d263e5a6485bb0d2e25a2881bc8e33f
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-30 14:17:31 -0400

    Update inline-hooks.tf

--- inline-hooks.tf
+++ inline-hooks.tf
@@ -1,10 +1,12 @@
 resource "okta_inline_hook" "validate_client_id" {
   name    = "Validate Client ID"
-  version = "1.0.1"
+  version = "1.0.2"
   type    = "com.okta.user.pre-registration"
+  status  = "ACTIVE"
 
   channel = {
     version = "1.0.0"
+    type    = "HTTP"
     uri     = "https://iatcoreapim.azure-api.net/okta/registration"
     method  = "POST"
   }
