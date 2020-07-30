8c6f7594538f98f95c9b147d29b205014efae9b8
paulverrone <66492837+paulverrone@users.noreply.github.com>
2020-07-30 16:25:07 -0400

    Update inline-hooks.tf

--- inline-hooks.tf
+++ inline-hooks.tf
@@ -1,6 +1,6 @@
 resource "okta_inline_hook" "validate_client_id" {
   name    = "Validate Client ID"
-  version = "1.0.2"
+  version = "1.0.0"
   type    = "com.okta.user.pre-registration"
   status  = "ACTIVE"
 
