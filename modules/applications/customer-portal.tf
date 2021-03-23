fdb57a0b345a3a7e017af5e33d31d24e2279fbf7
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-03-22 20:49:28 -0400

    Update customer-portal.tf

--- modules/applications/customer-portal.tf
+++ modules/applications/customer-portal.tf
@@ -3,7 +3,7 @@ resource "okta_app_oauth" "customer_portal" {
   label          = "Customer Portal"
   type           = "web"
   redirect_uris  = ["https://localhost:44369/signin-oidc", "https://localhost:44369/Customer", "https://oauth.pstmn.io/v1/callback"]
-  post_logout_redirect_uris = ["https://localhost:44369"]
+  post_logout_redirect_uris = ["https://localhost:44369/signout-callback-oidc"]
   login_uri      = "https://localhost:44369/signin-oidc"
   grant_types    = ["client_credentials", "authorization_code"]
   response_types = ["code"]
