895ec85904bd0a1b91dfee983cd4d26652ac066c
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-03-25 11:58:18 -0400

    Update variables.tf

--- modules/auth-servers/variables.tf
+++ modules/auth-servers/variables.tf
@@ -6,6 +6,6 @@ variable "Customer_Portal_App_Id" {
   description = "Customer Portal Application"
 }
 
-variable "org_name" {}
-variable "api_token" {}
-variable "base_url" {}
+#variable "org_name" {}
+#variable "api_token" {}
+#variable "base_url" {}
