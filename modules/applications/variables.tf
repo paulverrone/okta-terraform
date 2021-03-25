8a6e9885b668e097cd923574c401b98475dbf770
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-03-25 11:57:45 -0400

    Update variables.tf

--- modules/applications/variables.tf
+++ modules/applications/variables.tf
@@ -1,5 +1,5 @@
 variable "shared_admin_password_swa" {}
 variable "azure_apim_endpoint" {}
-variable "org_name" {}
-variable "api_token" {}
-variable "base_url" {}
+#variable "org_name" {}
+#variable "api_token" {}
+#variable "base_url" {}
