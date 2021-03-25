e67a6dfea6fe1049e6f989a6f0294bf76496c770
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-03-25 11:56:55 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,16 +18,16 @@ module "auth-servers" {
     source = ".//modules/auth-servers"
     OIDC_Client_Id = module.applications.oidc_superwidget_id
     Customer_Portal_App_Id = module.applications.customer_portal_app_id
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
+    #org_name = var.org_name
+    #base_url = var.base_url
+    #api_token = var.api_token
 }
     
 module "applications" {
     source = ".//modules/applications"
     shared_admin_password_swa = var.shared_admin_password_swa
     azure_apim_endpoint = var.azure_apim_endpoint
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
+    #org_name = var.org_name
+    #base_url = var.base_url
+    #api_token = var.api_token
 }
