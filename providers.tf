ac3e2e11408178ed8eb148856db70745218b20d6
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-07-28 10:47:39 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,7 +15,7 @@ provider "okta" {
 }
 
 module "applications" {
-    source = ".//modules/applications"
+    source = "./modules/applications"
     shared_admin_password_swa = var.shared_admin_password_swa
     azure_apim_endpoint = var.azure_apim_endpoint
     #org_name = var.org_name
@@ -24,7 +24,7 @@ module "applications" {
 }
 
 module "auth-servers" {
-    source = ".//modules/auth-servers"
+    source = "./modules/auth-servers"
     OIDC_Client_Id = module.applications.oidc_superwidget_id
     Customer_Portal_App_Id = module.applications.customer_portal_app_id
     #org_name = var.org_name
