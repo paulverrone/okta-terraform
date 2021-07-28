05beed878caf9ffcc561b1c354d3d9c2178b8d20
paulverrone <66492837+paulverrone@users.noreply.github.com>
2021-07-28 12:03:16 -0400

    Update users-and-groups.tf

--- users-and-groups.tf
+++ users-and-groups.tf
@@ -58,7 +58,7 @@ resource "okta_group_rule" "dmzadmin_grouprule" {
   status            = "ACTIVE"
   group_assignments = [ resource.okta_group.dmzadmin_group.id ]
   expression_type   = "urn:okta:expression:1.0"
-  expression_value  = "String.equals(user.department,\"DMZ Ops\")"
+  expression_value  = "user.department==\"DMZ Ops\""
 }
 
 # Find everyone group
